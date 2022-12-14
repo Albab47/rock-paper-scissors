d00a4ec9f7abded50ad841d8cfdd0f99e12d733d
Albab47 <115328782+Albab47@users.noreply.github.com>
2022-12-14 09:11:23 +0600

    Add game function that play 5 round.
    Added two new variable which stores scores and done some chages in play
    round function.

--- app.js
+++ app.js
@@ -9,20 +9,43 @@ function getComputerChoice() {
     }
 }
 
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You win! Paper beats Rock";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You Lose! Scissors beat Paper";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You win! Scissors beat Paper";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You Lose! Rock beats scissors";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You win! Rock beats scissors";
-    } else {
+    } else if (playerSelection === computerSelection) {
         return "It's a tie!";
-    }  
+    } else {
+        return "Enter the right choice (i.e: check your speling)";
+    }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Give your choice, Rock, Paper or Scissors?', '');
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+        console.log("Your score = " + playerScore);
+        console.log("Computer score = " + computerScore);
+    }
+}
+
+game();
